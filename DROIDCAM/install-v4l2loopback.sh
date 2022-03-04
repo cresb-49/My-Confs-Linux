48024731f7cbec311cc2468f6e16dec74835401c
benjamin <carlosbpac@gmail.com>
2022-03-03 23:20:27 -0600

    Vista al usaurio

--- DROIDCAM/install-v4l2loopback.sh
+++ DROIDCAM/install-v4l2loopback.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
 #UBIACION EN CARPETA TMP
+echo "ACCEDIENDO AL DIRECTORIO TMP"
 cd /tmp/
 
 #DESCARGA DE DROID CAM
+echo "DESCARGANDO DROIDCAM de Dev47Apps"
 wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.2.zip
 
 #DESCOMPRECION DEL ARCHIVO
+echo "DESCOMPRIMIENDO DROID CAM"
 unzip droidcam_latest.zip -d droidcam
 
 #UBICAMOS LA CARPETA DROID CAM
+echo "ACCEDEIENDO A DIRECTORIO DROIDCAM EN TMP"
 cd droidcam
 
 #INSTALAMOS LA DEPENDECIA DE VIDEO v4l2loopback
+echo "INSTALANDO v4l2loopback"
 ./install-video
--- DROIDCAM/installDroidCam.sh
+++ DROIDCAM/installDroidCam.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
 
 #UBIACION EN CARPETA TMP
+echo "ACCEDIENDO AL DIRECTORIO TMP"
 cd /tmp/
 
 #DESCARGA DE DROID CAM
+echo "DESCARGANDO DROIDCAM de Dev47Apps"
 wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.2.zip
 
 #DESCOMPRECION DEL ARCHIVO
+echo "DESCOMPRIMIENDO DROID CAM"
 unzip droidcam_latest.zip -d droidcam
 
 #UBICAMOS LA CARPETA DROID CAM
+echo "ACCEDEIENDO A DIRECTORIO DROIDCAM EN TMP"
 cd droidcam
 
 #EJECUTAMOS EL INSTALADOR DEL CLIENTE DE DROID CAM
+echo "INSTALANDO cliente droidcam"
 ./install-client
 
 #INSTALAMOS LA DEPENDECIA DE VIDEO v4l2loopback
+echo "INSTALANDO v4l2loopback"
 ./install-video
